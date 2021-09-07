dc59b7fc3b705db0e35bfee818909a1b377a8792
Viktor-Stojkov <viktor.stojkov93@gmail.com>
2021-09-07 14:21:21 +0200

    Advance Homeworks

---
+++ Homework05/Bonus/Users/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bonus.Users
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+        }
+
+        public string PrintPerson()
+        {
+           return $"{FirstName} {LastName} {Age}";
+        }
+    }
+}
